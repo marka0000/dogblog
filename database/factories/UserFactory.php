159c7e177941a7b45e53f4cb00ff6bc3970bed3b
Kirill <marka0000@list.ru>
2021-05-31 11:42:43 +0300

    Add data for tables - users, posts, comments

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'nickname' => $this->faker->firstName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'created_at' => $this->faker->dateTime(),
+            'updated_at' => $this->faker->dateTime(),
         ];
     }
 
