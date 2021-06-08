18db281cd961386dbd02978667af67abf90082ac
Kirill <marka0000@list.ru>
2021-06-08 17:09:41 +0300

    auth with sanctum - register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'nickname' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'nickname' => $fields['nickname'],
+            'email' => $fields['email'],
+            'password' => $fields['password'],
+        ]);
+
+        $token = $user->createToken('dogblog')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::create([
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response, 201);
+    }
+
+
+}
--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,75 +4,91 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-    public function testPostsShowSuccess()
+    public function testRegisterApi()
     {
-        $this->get('api/posts')
-            ->assertStatus(200);
-    }
+        $header = ['Accept: application/json'];
 
-    public function testPostShowSuccess()
-    {
-        $this->get('api/posts/' . $this->getIntSuccess())
-            ->assertStatus(200);
-    }
-
-    public function testPostShowMissing()
-    {
-        $this->get('api/posts/' . $this->getIntMissing())
-            ->assertStatus(404);
-    }
-
-    public function testPostCreatedSuccess()
-    {
         $data = [
-            'title' => 'Dog post blog',
-            'sub_title' => 'Post about very small dog',
-            'content' => 'The little dog loves to play ball',
-            'user_id' => $this->getIntSuccess(),
+            'name' => Str::random(6),
+            'nickname' => Str::random(8),
+            'email' => Str::random(6) . '@test.com',
+            'password' => '$2y$10$92oXUNpkkO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ];
-        $this->post('api/posts', $data)
+
+        $this->post('api/register', $data, $header)
             ->assertStatus(201);
     }
 
-    public function testPostUpdatedSuccess()
-    {
-        $data = [
-            'title' => 'Title updated after test',
-            'sub_title' => 'Title updated after test',
-            'content' => 'Title updated after test',
-            'user_id' => $this->getIntSuccess(),
-        ];
-        $this->put('api/posts/4', $data)
-            ->assertStatus(200);
-    }
-
-    public function testPostUpdatedMissing()
-    {
-        $data = [
-            'title' => 'Title updated after test',
-            'sub_title' => 'Title updated after test',
-            'content' => 'Title updated after test',
-            'user_id' => $this->getIntSuccess(),
-        ];
-        $this->put('api/posts/' . $this->getIntMissing(), $data)
-            ->assertStatus(404);
-    }
-
-    public function testPostDeletedSucces()
-    {
-        $this->delete('api/posts/6')
-            ->assertStatus(204);
-    }
-
-    public function testPostDeletedMissing()
-    {
-        $this->delete('api/posts/' . $this->getIntMissing())
-            ->assertStatus(404);
-    }
+//    public function testPostsShowSuccess()
+//    {
+//        $this->get('api/posts')
+//            ->assertStatus(200);
+//    }
+//
+//    public function testPostShowSuccess()
+//    {
+//        $this->get('api/posts/' . $this->getIntSuccess())
+//            ->assertStatus(200);
+//    }
+//
+//    public function testPostShowMissing()
+//    {
+//        $this->get('api/posts/' . $this->getIntMissing())
+//            ->assertStatus(404);
+//    }
+//
+//    public function testPostCreatedSuccess()
+//    {
+//        $data = [
+//            'title' => 'Dog post blog',
+//            'sub_title' => 'Post about very small dog',
+//            'content' => 'The little dog loves to play ball',
+//            'user_id' => $this->getIntSuccess(),
+//        ];
+//        $this->post('api/posts', $data)
+//            ->assertStatus(201);
+//    }
+//
+//    public function testPostUpdatedSuccess()
+//    {
+//        $data = [
+//            'title' => 'Title updated after test',
+//            'sub_title' => 'Title updated after test',
+//            'content' => 'Title updated after test',
+//            'user_id' => $this->getIntSuccess(),
+//        ];
+//        $this->put('api/posts/4', $data)
+//            ->assertStatus(200);
+//    }
+//
+//    public function testPostUpdatedMissing()
+//    {
+//        $data = [
+//            'title' => 'Title updated after test',
+//            'sub_title' => 'Title updated after test',
+//            'content' => 'Title updated after test',
+//            'user_id' => $this->getIntSuccess(),
+//        ];
+//        $this->put('api/posts/' . $this->getIntMissing(), $data)
+//            ->assertStatus(404);
+//    }
+//
+//    public function testPostDeletedSucces()
+//    {
+//        $this->delete('api/posts/6')
+//            ->assertStatus(204);
+//    }
+//
+//    public function testPostDeletedMissing()
+//    {
+//        $this->delete('api/posts/' . $this->getIntMissing())
+//            ->assertStatus(404);
+//    }
 
     private function getIntSuccess()
     {
